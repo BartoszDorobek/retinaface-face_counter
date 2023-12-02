2ffb0ebfc57caf057e61a08b1492fdb3e5eef5a1
Bartosz Gracjan Dorobek <b.dorobek@stud.elka.pw.edu.pl>
2023-12-02 17:11:21 +0000

    add counter, change test for original

--- test.py
+++ test.py
@@ -4,12 +4,14 @@ import cv2
 import os
 import numpy as np
 import tensorflow as tf
+import time
 
 from modules.models import RetinaFaceModel
-from modules.utils import (set_memory_growth, load_yaml, pad_input_image,
-                           recover_pad_output, crop_image)
+from modules.utils import (set_memory_growth, load_yaml, draw_bbox_landm,
+                           pad_input_image, recover_pad_output, crop_image)
 
-flags.DEFINE_string('cfg_path', './configs/retinaface_mbv2.yaml',
+
+flags.DEFINE_string('cfg_path', './configs/retinaface_res50.yaml',
                     'config file path')
 flags.DEFINE_string('gpu', '0', 'which gpu to use')
 flags.DEFINE_string('img_path', '', 'path to input image')
@@ -20,6 +22,7 @@ flags.DEFINE_float('down_scale_factor', 1.0, 'down-scale factor for inputs')
 
 
 def main(_argv):
+    # init
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
     os.environ['CUDA_VISIBLE_DEVICES'] = FLAGS.gpu
 
@@ -30,9 +33,11 @@ def main(_argv):
 
     cfg = load_yaml(FLAGS.cfg_path)
 
+    # define network
     model = RetinaFaceModel(cfg, training=False, iou_th=FLAGS.iou_th,
                             score_th=FLAGS.score_th)
 
+    # load checkpoint
     checkpoint_dir = './checkpoints/' + cfg['sub_name']
     checkpoint = tf.train.Checkpoint(model=model)
     if tf.train.latest_checkpoint(checkpoint_dir):
@@ -60,15 +65,16 @@ def main(_argv):
                              interpolation=cv2.INTER_LINEAR)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
+        # pad input image to avoid unmatched shape problem
         img, pad_params = pad_input_image(img, max_steps=max(cfg['steps']))
 
+        # run model
         outputs = model(img[np.newaxis, ...]).numpy()
 
+        # recover padding effect
         outputs = recover_pad_output(outputs, pad_params)
 
-        head_count = len(outputs)
-        print(f"[*] Found {head_count} faces in {FLAGS.img_path}")
-
+        # draw and save results        
         print(f"[*] saving {len(outputs)} results")
         for prior_index in range(len(outputs)):
             crop_img = crop_image(img_raw, outputs[prior_index], img_height_raw, img_width_raw)
@@ -92,24 +98,27 @@ def main(_argv):
                                  interpolation=cv2.INTER_LINEAR)
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
+            # pad input image to avoid unmatched shape problem
             img, pad_params = pad_input_image(img, max_steps=max(cfg['steps']))
 
+            # run model
             outputs = model(img[np.newaxis, ...]).numpy()
 
+            # recover padding effect
             outputs = recover_pad_output(outputs, pad_params)
 
-            head_count = len(outputs)
-            print(f"[*] Found {head_count} faces in webcam frame")
-
+            # draw results
             for prior_index in range(len(outputs)):
                 draw_bbox_landm(frame, outputs[prior_index], frame_height,
                                 frame_width)
 
+            # calculate fps
             fps_str = "FPS: %.2f" % (1 / (time.time() - start_time))
             start_time = time.time()
             cv2.putText(frame, fps_str, (25, 25),
                         cv2.FONT_HERSHEY_DUPLEX, 0.75, (0, 255, 0), 2)
 
+            # show frame
             cv2.imshow('frame', frame)
             if cv2.waitKey(1) == ord('q'):
                 exit()
